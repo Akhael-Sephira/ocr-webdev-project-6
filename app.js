01d8d15ef14f0ff0810351a4b84d35db757b83d6
Akhael <akhaeldelacroix@gmail.com>
2022-11-13 18:28:58 +0100

    Adding morgan Adding CORS_ORIGIN to .env

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const sanitize = require('express-mongo-sanitize');
 const path = require('path');
+const morgan = require('morgan');
+const fs = require('fs');
 
 const authRoute = require('./routes/auth');
 const saucesRoute = require('./routes/sauces');
 const authorize = require('./middleware/auth');
+const { env } = require('process');
 
 const app = express();
 
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => { console.log('Connection to MongoDB: Successful') })
 .catch(() => { console.log('Connection to MongoDB: Failed') });
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET, POST, PUT, DELETE',
+}));
 
 app.use(express.json());
 
 app.use(sanitize({
     replaceWith: '_'
 }));
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), {flags: 'a'});
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
