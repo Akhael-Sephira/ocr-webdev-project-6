39a13d168e625dcd4ae7896166861776178cf58f
Akhael <akhaeldelacroix@gmail.com>
2022-11-17 14:38:59 +0100

    Create logs folder if doesn't exist.

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.use(sanitize({
     replaceWith: '_'
 }));
 
+
+if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+    fs.mkdirSync(path.join(__dirname, 'logs'));
+}
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), {flags: 'a'});
 app.use(morgan('combined', { stream: accessLogStream }));
 
